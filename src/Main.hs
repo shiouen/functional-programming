9a4eab0bf44fe26f583492ad7ea0aca8f84a98bc
Olivier Van Aken <olivier.va@outlook.com>
2016-08-31 07:17:37 +0200

    FInished step 2

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,14 @@
 module Main where
 
 import System.IO
+import Data.List (sort, transpose)
 import Data.List.Split (splitOn)
 import qualified Data.Time.Calendar as T (Day(..), fromGregorian)
 
 type FileName = String
 type CSVString = String
 
-data Series = TimeSeries { tseries :: [Float] } | NumberSeries { nseries :: [Float] }
+data Series = TimeSeries { tseries :: [T.Day] } | NumberSeries { nseries :: [Float] }
     deriving Show
 
 data Bars = Bars {
@@ -17,7 +18,7 @@ data Bars = Bars {
     low :: Series,
     close :: Series,
     adjclose :: Series,
-    volumes :: Series
+    volume :: Series
 } deriving Show
 
 -- Name : readCsv
@@ -43,7 +44,6 @@ parseCsv csvString =
 stringToInteger :: String -> Integer
 stringToInteger str = read str :: Integer
 
-
 -- Name : makeDay
 -- Input: String
 -- Output: T.Day
@@ -56,6 +56,42 @@ makeDay date =
         day = fromIntegral (stringToInteger (dateData !! 2))
     in T.fromGregorian year month day
 
+-- Name : stringToFloat
+-- Input : String
+-- Output : Float
+-- Uses : read
+stringToFloat :: String -> Float
+stringToFloat string = read string :: Float
+
+makeSeries :: Char -> [String] -> Series
+makeSeries option series
+    | option == 'N' = NumberSeries { nseries = map stringToFloat series }
+    | option == 'T' = TimeSeries { tseries = map makeDay series }
+
+-- Name : makeBars
+-- Input : CSVString
+-- Output : Bars
+-- Uses : map, reverse, parseCsv, transpose
+makeBars :: CSVString -> Bars
+makeBars csvString =
+    let csvData = parseCsv csvString
+        transposedCsvData = map reverse (transpose csvData)
+        dates = makeSeries 'T' (transposedCsvData !! 0)
+        opens = makeSeries 'N' (transposedCsvData !! 1)
+        highs = makeSeries 'N' (transposedCsvData !! 2)
+        lows = makeSeries 'N' (transposedCsvData !! 3)
+        closes = makeSeries 'N' (transposedCsvData !! 4)
+        adjcloses = makeSeries 'N' (transposedCsvData !! 6)
+        volumes = makeSeries 'N' (transposedCsvData !! 5)
+    in Bars {
+        date = dates,
+        open = opens,
+        high = highs,
+        low = lows,
+        close = closes,
+        adjclose = adjcloses,
+        volume = volumes
+    }
 
 -- main
 main :: IO()
@@ -63,6 +99,6 @@ main = do
     hSetBuffering stdout NoBuffering
     csvString <- readCsv "./data/msft.csv"
 
-    let csvData = parseCsv csvString
-    print(csvData)
+    let bars = makeBars csvString
+    print(bars)
 
